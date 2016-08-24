b02926274b752e68820b5ae3fe63170f24f7abae
WiWi33 <oxbow.33@hotmail.fr>
2016-08-24 23:01:32 +0200

    Add files via upload

--- Quests/Johto/Elite4Johto.lua
+++ Quests/Johto/Elite4Johto.lua
@@ -2,7 +2,7 @@
 -- This work is free. You can redistribute it and/or modify it under the
 -- terms of the Do What The Fuck You Want To Public License, Version 2,
 -- as published by Sam Hocevar. See the COPYING file for more details.
--- Quest: @WiWi__33
+-- Quest: @WiWi__33 & Melt
 
 
 local sys    = require "Libs/syslib"
