90714acba12b3659f7bff2e94199e18df1dbdbf6
m1l4 <m1l4@opentrash.com>
2017-09-06 00:52:57 +0200

    [Fix] Route 19 SurfPkm - fixed an issue, where it wouldn't be tried to teach the 6th pkm surf

--- Quests/Kanto/SoulBadgeQuest.lua
+++ Quests/Kanto/SoulBadgeQuest.lua
@@ -10,7 +10,7 @@ local game   = require "Libs/gamelib"
 local Quest  = require "Quests/Quest"
 local Dialog = require "Quests/Dialog"
 
-local name		  = 'Sould Badge'
+local name		  = 'Soul Badge'
 local description = 'Fuchsia City'
 local level = 40
 
@@ -209,7 +209,7 @@ function SoulBadgeQuest:Route19()
 		return moveToMap("Fuchsia City Stop House")
 	elseif hasItem("HM03 - Surf") then
 		if not game.hasPokemonWithMove("Surf") then
-			if self.pokemonId < getTeamSize() then					
+			if self.pokemonId <= getTeamSize() then
 				useItemOnPokemon("HM03 - Surf", self.pokemonId)
 				log("Pokemon: " .. self.pokemonId .. " Try Learning: HM03 - Surf")
 				self.pokemonId = self.pokemonId + 1
