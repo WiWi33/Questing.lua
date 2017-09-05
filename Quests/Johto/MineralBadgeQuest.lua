27bdcd69a648bcad049b41d4b0aac70cd236b4ee
m1l4 <m1l4@opentrash.com>
2017-09-05 17:26:06 +0200

    [Fix] Rods - fixed an endless loop, when pre rod wasn't obtained

--- Quests/Johto/MineralBadgeQuest.lua
+++ Quests/Johto/MineralBadgeQuest.lua
@@ -69,7 +69,7 @@ function MineralBadgeQuest:Route40()
 end
 
 function MineralBadgeQuest:OlivineCity()
-	if BUY_RODS and not hasItem("Super Rod") and getMoney() >= 75000 then
+	if BUY_RODS and hasItem("Good Rod") and not hasItem("Super Rod") and getMoney() >= 75000 then
 		--go to fising guru's map, if you have enough money and want to buy the super rod
 		return moveToMap("Olivine House 1")
 	elseif self:needPokecenter() or not game.isTeamFullyHealed() or not self.registeredPokecenter == "Pokecenter Olivine City" then
@@ -84,7 +84,7 @@ end
 
 function MineralBadgeQuest:OlivineHouse1()
 	--talk to the even better fishing guru
-	if not hasItem("Super Rod") then return talkToNpcOnCell(0, 10)
+	if hasItem("Good Rod") and not hasItem("Super Rod") then return talkToNpcOnCell(0, 10)
 	--leave when rod obtained
 	else return moveToMap("Olivine City") end
 end
